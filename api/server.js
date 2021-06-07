ff393df8af81882d53fd20c1decea44db1529c82
Todd Pressley <todd.pressley1@gmail.com>
2021-06-07 15:47:18 -0400

    make note of api TODOs

--- api/server.js
+++ api/server.js
@@ -1,16 +1,22 @@
-const cors =  require('cors')
-const express =  require('express')
+/**
+ * @TODO: revamp api src to use es6 via babel, babel-preset-env
+ */
+
+const cors = require('cors');
+const express = require('express');
 
 const app = express();
 const PORT = 8080;
 
 const db = require('./models');
 
-app.use(cors({
-    origin: 'http://localhost:8081'
-}))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(
+  cors({
+    origin: 'http://localhost:8081',
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 require('./routes/tutorial.routes')(app);
 
@@ -19,5 +25,5 @@ db.sequelize.sync({ force: true }).then(() => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
