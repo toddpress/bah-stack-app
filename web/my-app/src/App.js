6dc74ebe1e5de6b6d724e34027ff1045fc6df7a6
Todd Pressley <todd.pressley1@gmail.com>
2021-06-07 17:30:40 -0400

    add basic app-specific components

--- web/my-app/src/App.js
+++ web/my-app/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Link, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import AddTutorial from './components/AddTutorial'
+import Tutorial from './components/Tutorial'
+import TutorialsList from './components/TutorialsList'
+import logo from './logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to={'/tutorials'}>Tutorials</Link>
+            </li>
+            <li>
+              <Link to={'/add'}>Add Tutorial</Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <Switch>
+            <Route exact path={['/', '/tutorials']} component={TutorialsList} />
+            <Route exact path={'/add'} component={AddTutorial} />
+            <Route path={'/tutorials/:id'} component={Tutorial} />
+          </Switch>
+        </main>
+      </div>
+    </Router>
   );
 }
 
---
+++ web/my-app/src/components/AddTutorial.js
@@ -0,0 +1,3 @@
+export default function AddTutorial() {
+    return (<div>Add Tutorial</div>)
+}
